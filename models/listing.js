cb1b7f565666cb6fbfe57c2a73f3cc7117f423fc
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-01 17:54:59 +0530

    Add initial files and configurations for various modules

---
+++ models/listing.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: { type: String, required: true },
+    description: String,
+    image: {
+        filename: { type: String, default: "listingimage" },
+        url: { 
+            type: String, 
+            default: "https://images.unsplash.com/photo-1742426426875-4c16b5f4e95e?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set: (url) => url === "" ? "https://images.unsplash.com/photo-1742426426875-4c16b5f4e95e?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : url
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
