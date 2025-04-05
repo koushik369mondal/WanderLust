878e6fe2bdc0fcfffc955b6fbef8008a1cb628b5
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-05 19:56:33 +0530

    Add review validation and schema to enhance review functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -35,7 +35,17 @@ app.get("/", (req, res) => {
 });
 
 const validateListing = (req, res, next) => {
-    let {error} = listingSchema.validate(req.body);
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -70,7 +80,7 @@ app.get(
 
 //Create Route
 app.post(
-    "/listings", 
+    "/listings",
     validateListing,
     wrapAsync(async (req, res, next) => {
         const newListing = new Listing(req.body.listing);
@@ -112,17 +122,21 @@ app.delete(
 );
 
 // Reviews -> Post Route
-app.post("/listings/:id/reviews", async (req, res) =>{
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
+app.post(
+    "/listings/:id/reviews",
+    validateReview,
+    wrapAsync(async (req, res) => {
+        let listing = await Listing.findById(req.params.id);
+        let newReview = new Review(req.body.review);
 
-    listing.reviews.push(newReview);
+        listing.reviews.push(newReview);
 
-    await newReview.save();
-    await listing.save();
+        await newReview.save();
+        await listing.save();
 
-    res.redirect(`/listings/${listing._id}`);
-});
+        res.redirect(`/listings/${listing._id}`);
+    })
+);
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
