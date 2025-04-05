a5f3415a4198bb2de3d8a8a6f7a962f36032f2d7
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-05 23:34:35 +0530

    ensure review deletion on listing removal

--- models/listing.js
+++ models/listing.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
     title: { type: String, required: true },
     description: String,
     image: {
         filename: { type: String, default: "listingimage" },
-        url: { 
-            type: String, 
-            default: "https://images.unsplash.com/photo-1742426426875-4c16b5f4e95e?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            set: (url) => url === "" ? "https://images.unsplash.com/photo-1742426426875-4c16b5f4e95e?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : url
-        }
+        url: {
+            type: String,
+            default:
+                "https://images.unsplash.com/photo-1742426426875-4c16b5f4e95e?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set: (url) =>
+                url === ""
+                    ? "https://images.unsplash.com/photo-1742426426875-4c16b5f4e95e?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    : url,
+        },
     },
     price: Number,
     location: String,
@@ -21,6 +26,12 @@ const listingSchema = new Schema({
             ref: "Review",
         },
     ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
