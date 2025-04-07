4cc149017d6f0762ee654e3a16475ba86bf320e4
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-08 01:27:28 +0530

    Refactor post routes: move route handlers to post.js and update server.js to use the new routing structure

--- classroom/server.js
+++ classroom/server.js
@@ -1,34 +1,14 @@
 const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
+const posts = require("./routes/post.js");
 
 app.use("/users", users);
+app.use("/posts", posts)
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root route");
 });
-
-
-//POST
-//Index
-app.get("/posts", (req, res) => {
-    res.send("Hi, I am posts route");
-});
-
-//Show 
-app.get("/posts/:id", (req, res) => {
-    res.send(`GET for show posts id`);
-});
-
-//POST 
-app.post("/posts", (req, res) => {
-    res.send("POST for posts");
-})
-
-//DELETE 
-app.delete("/posts/:id", (req, res) => {
-    res.send(`DELETE for posts id`);
-})
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
