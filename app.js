0f2461c1dac33f70acf958a0450ce1d2b0a84196
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-07 22:25:48 +0530

    Refactor review route to include validation middleware and improve error handling; reorganize imports for clarity

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema, reviewSchema } = require("./schema.js");
-const Review = require("./models/review.js");
+
 
 const listings = require("./routes/listing.js");
 const reviews = require("./models/review.js");
@@ -37,32 +34,8 @@ app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
 
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
-
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
-
-
-// app.get("/testListing", async (req, res) => {
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         country: "India",
-//     });
-//     await sampleListing.save();
-//     console.log("Saved sample");
-//     res.send("Successful testing");
-// });
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
