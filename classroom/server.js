7eee56b87d9c56204cf1252b67c57f7159801b6f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-08 19:51:30 +0530

    npm i cookie-parser

--- classroom/server.js
+++ classroom/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser())
+app.use("/users", users);
+app.use("/posts", posts);
 
 app.get("/getcookies", (req, res) => {
     res.cookie("greet", "namaste");
@@ -10,11 +15,9 @@ app.get("/getcookies", (req, res) => {
 });
 
 app.get("/", (req, res) => {
+    console.dir(req.cookies);
     res.send("Hi, I am root route");
 });
-
-app.use("/users", users);
-app.use("/posts", posts);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
