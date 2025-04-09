77165ab1d2d433f21d9e4f4fee62bc0078565ce6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-10 00:18:26 +0530

    Enhance flash message handling on registration to display success and error messages

--- classroom/server.js
+++ classroom/server.js
@@ -21,12 +21,18 @@ app.use(flash());
 app.get("/register", (req, res) => {
     let { name = "anonymous" } = req.query;
     req.session.name = name;
-    req.flash("success", "user register successfully");
+    if(name === "anonymous") {
+        req.flash("error", "user not registered");
+    }
+    else {
+        req.flash("success", "user registered successfully");
+    }
     res.redirect("/hello");
-});
+}); 
 
 app.get("/hello", (req, res) => {
-    res.locals.messages = req.flash("success");
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
     res.render("page.ejs", {name: req.session.name});
 });
 
