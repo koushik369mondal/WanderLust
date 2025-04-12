39515917915e7b1774e226dc1d97a9974c7e055f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-12 15:43:35 +0530

    Add demo user route and fix user schema plugin reference

--- app.js
+++ app.js
@@ -64,6 +64,16 @@ app.use((req, res, next) => {
     next();
 });
 
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "delta-student",
+    });
+
+    let registerUser = await User.register(fakeUser, "helloworld");
+    res.send(registerUser);
+});
+
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 
