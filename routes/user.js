a3deb71d8bbe1bb78e69943c6674e51a577328ab
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-12 17:49:54 +0530

    Wrap signup route logic in try-catch for improved error handling

--- routes/user.js
+++ routes/user.js
@@ -10,12 +10,17 @@ router.get("/signup", (req, res) => {
 router.post(
     "/signup",
     wrapAsync(async (req, res) => {
-        let { username, email, password } = req.body;
-        const newUser = new User({ email, username });
-        const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.flash("success", "Welcome to Wanderlust!");
-        res.redirect("/listings");
+        try {
+            let { username, email, password } = req.body;
+            const newUser = new User({ email, username });
+            const registeredUser = await User.register(newUser, password);
+            console.log(registeredUser);
+            req.flash("success", "Welcome to Wanderlust!");
+            res.redirect("/listings");
+        } catch (e) {
+            req.flash("error", e.message);
+            res.redirect("/signup");
+        }
     })
 );
 
