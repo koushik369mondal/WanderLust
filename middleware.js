b027657b0c33933a4a9e9005acd8117fa33e594c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-13 18:23:54 +0530

    Add user authentication check and update navbar for logout functionality

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log(req.user);
     if (!req.isAuthenticated()) {
         req.flash("error", "You must be logged in to create a listing!");
         return res.redirect("/login");
