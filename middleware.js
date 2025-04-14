47cd56313e641eca38af495e9e6db3381d98663f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-14 16:37:20 +0530

    Fix missing next() call in isOwner middleware to ensure proper flow

--- middleware.js
+++ middleware.js
@@ -22,4 +22,5 @@ module.exports.isOwner = async (req, res, next) => {
         req.flash("error", "You are not the owner of this listing!");
         return res.redirect(`/listings/${id}`);
     }
-}
+    next();
+};
