8d2ff60e96836dcf9afcf7c1a8d717f648d930c0
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-15 21:28:43 +0530

    npm i dotenv

--- routes/listing.js
+++ routes/listing.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingsController = require("../controllers/listings.js");
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router
     .route("/")
@@ -14,9 +15,9 @@ router
     //     validateListing,
     //     wrapAsync(listingsController.createListing)
     // );
-    .post((req, res) => {
-        res.send(req.body);
-    })
+    .post(upload.single('listing[image]'), (req, res) => {
+        res.send(req.file);
+    });
 
 // New Route
 router.get("/new", isLoggedIn, listingsController.renderNewForm);
