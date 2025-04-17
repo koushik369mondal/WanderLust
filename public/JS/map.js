429504da16a8963a0fd7c44903eb59259279ef4f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-17 23:34:01 +0530

    Refactor map.js and show.ejs to use dynamic coordinates for map initialization and marker placement

--- public/JS/map.js
+++ public/JS/map.js
@@ -2,10 +2,10 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: "map", // container ID
-    center: [89.0418, 26.7995], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9, // starting zoom
 });
 
 const marker = new mapboxgl.Marker()
-.setLngLat([12.554729, 55.70651]) //Listing.geometry.coordinates
+.setLngLat(coordinates) //Listing.geometry.coordinates
 .addTo(map);
