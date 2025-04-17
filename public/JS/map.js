1fc7d9b32498f8eb52a45da14e27c873cfc84248
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-17 23:25:17 +0530

    Add marker to map initialization in map.js

--- public/JS/map.js
+++ public/JS/map.js
@@ -5,3 +5,7 @@ const map = new mapboxgl.Map({
     center: [89.0418, 26.7995], // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9, // starting zoom
 });
+
+const marker = new mapboxgl.Marker()
+.setLngLat([12.554729, 55.70651]) //Listing.geometry.coordinates
+.addTo(map);
