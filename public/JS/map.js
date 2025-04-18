aa000ea8cb6e74b87db07058dc8484104ae9d5f1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-18 11:51:57 +0530

    Fix marker popup to display listing title instead of location

--- public/JS/map.js
+++ public/JS/map.js
@@ -10,7 +10,7 @@ const marker = new mapboxgl.Marker({ color: "red" })
     .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
     .setPopup(
         new mapboxgl.Popup({ offset: 25 }).setHTML(
-            `<h4>${listing.location}</h4><p>Exact location provided after booking</p>`
+            `<h4>${listing.title}</h4><p>Exact location will be provided after booking</p>`
         )
     )
     .addTo(map);
