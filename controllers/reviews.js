50df236ebae8922cfb50357f62cc4d1efcb9d559
Your Name <muskana1216@gmail.com>
2025-10-06 15:22:44 +0530

    updated  ui

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -11,7 +11,7 @@ module.exports.createReview = async (req, res) => {
     await newReview.save();
     await listing.save();
     req.flash("success", "New review created!");
-    res.redirect(`/listings/${listing._id}`);
+    res.redirect(`/listings/${listing._id}`);  
 };
 
 module.exports.destroyReview = async (req, res) => {
